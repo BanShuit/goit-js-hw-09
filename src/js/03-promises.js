f0f82588d21154804c63c90c65b5799081d80595
BanShuit <tuna125@o2.pl>
2024-04-15 20:47:53 +0200

    not yet

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+const delayInput = form.querySelector('input[name="delay"]');
+const stepInput = form.querySelector('input[name="step"]');
+const amountInput = form.querySelector('input[name="amount"]');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const delay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        window.Notiflix.Notify.Success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        window.Notiflix.Notify.Failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
