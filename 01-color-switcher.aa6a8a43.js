2727409d131d33b210de6f24286bf0b4b6fab9ee
BanShuit <140429293+BanShuit@users.noreply.github.com>
2024-04-15 17:24:56 +0000

    Deploying to gh-pages from @ BanShuit/goit-js-hw-09@9049413c1acc22474cd3ab4f422bc99d4de4dbed 🚀

---
+++ 01-color-switcher.aa6a8a43.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),e=null;function a(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.backgroundColor=t}t.addEventListener("click",(function(){clearInterval(e),e=setInterval(a,750)})),n.addEventListener("click",(function(){clearInterval(e)}))}();
+//# sourceMappingURL=01-color-switcher.aa6a8a43.js.map
